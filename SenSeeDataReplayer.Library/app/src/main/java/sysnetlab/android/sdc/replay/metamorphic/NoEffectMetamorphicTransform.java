3549f54f7dfc09072fc22c6a9b6badd240b51469
Marco <maroc_mlgpro@hotmail.com>
2015-01-08 15:56:49 -0500

    Multiply by 2 Transform
    attempting to multiply "inputData" by 2

--- SenSeeDataReplayer.Library/app/src/main/java/sysnetlab/android/sdc/replay/metamorphic/NoEffectMetamorphicTransform.java
+++ SenSeeDataReplayer.Library/app/src/main/java/sysnetlab/android/sdc/replay/metamorphic/NoEffectMetamorphicTransform.java
@@ -1,15 +1,35 @@
 package sysnetlab.android.sdc.replay.metamorphic;
 
 import java.util.List;
+import java.util.*;
 
-/**
- * Created by kosta on 12/14/2014.
- */
+
 public class NoEffectMetamorphicTransform implements MetamorphicTransform {
 
     public List<float[]> transform(List<float[]> inputData)
     {
         //just to show how this could look like
+
+        //for(float num : inputData) {
+        //    num = num*2;
+        //}
+
+        //for(float i = 0; i < inputData.size(); i++){
+        //        inputData[i] = inputData[i] * 2;
+       // }
+
+
+        //loop to get the each array of float values of the list
+
+        for(int i=0; i<inputData.size();i++) {
+            float[] array = inputData.get(i);
+            for(int j =0;j<array.length;j++) {
+                array[j] = array[j] *2;
+            }
+            inputData.set(i, array);
+        }
         return inputData;
+
+
     }
 }
